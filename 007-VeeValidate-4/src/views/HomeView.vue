f8317f353d852a99b46e871eda8295d98eb4a2a6
RohimChou <rohim.chou@gmail.com>
2022-10-20 13:07:23 +0800

    refs in script setup

--- 007-VeeValidate-4/src/views/HomeView.vue
+++ 007-VeeValidate-4/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import { onMounted } from 'vue';
 import { Form, Field, ErrorMessage } from 'vee-validate'
 import AllRules from '@vee-validate/rules'
 import { defineRule } from 'vee-validate'
 import { configure } from 'vee-validate';
+import { computed, ref } from "vue";
 
 // import rules
 Object.keys(AllRules).forEach(rule => {
@@ -19,7 +19,18 @@ configure({
 });
 
 const mySubmit = (formValues) => {
+  console.log("mySubmit");
   console.log(formValues);
+}
+
+const myForm = ref(null);
+const myInvalidSubmit = (formValues) => {
+  console.log("myInvalidSubmit, focus on first error");
+  console.log(formValues);
+  const { errors, evt, results, values } = formValues;
+
+  // <form>
+  console.log(myForm.value);
 }
 </script>
 
@@ -27,15 +38,15 @@ const mySubmit = (formValues) => {
   <main>
     <div id="div1">
       Form1
-      <Form v-slot="{ handleSubmit }" as="div">
-        <form @submit="handleSubmit($event, mySubmit)" >
+      <Form v-slot="{ handleSubmit }" as="div" @invalid-submit="myInvalidSubmit">
+        <form @submit="handleSubmit($event, mySubmit)" ref="myForm">
           <label for="username">UserName: </label>
           <Field type="text" name="username" rules="required"></Field>
           <ErrorMessage style="color:red" name="username"></ErrorMessage>
 
           <label for="birthyear">BirthYear: </label>
-          <Field type="text" name="birthyear" v-slot="abc">
-          </Field>
+          <Field type="number" name="birthyear" rules="required|between:1,50"></Field>
+          <ErrorMessage style="color:red" name="birthyear"></ErrorMessage>
 
           <button id="btn1">this will submit</button>
           <button id="btn2" type="button">this won't</button>
