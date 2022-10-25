000d33c87e4f4e2690612f469bd25b8fbb38f891
RohimChou <rohim.chou@gmail.com>
2022-10-25 12:54:28 +0800

    vue-router Clean up files

--- 006-vue-router/src/App.vue
+++ 006-vue-router/src/App.vue
@@ -1,19 +1,17 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 import { useRouter } from 'vue-router'
 let route = useRouter();
-  // Note: Inside of a Vue instance, you have access to the router instance as $router. 
-  // You can therefore call this.$router.push.
-  function abc() {
-    route.push({ path: '/test' });
-  }
+
+// Note: Inside of a Vue instance, you have access to the router instance as $router. 
+// You can therefore call this.$router.push.
+function abc() {
+  route.push({ path: '/test' });
+}
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
       <button @click="abc">Test</button>
--- 006-vue-router/src/views/TestView.vue
+++ 006-vue-router/src/views/TestView.vue
@@ -1,3 +1,14 @@
 <template>
     <h1>Test Page</h1>
+    <p>{{ aaa }}</p>
 </template>
+
+<script>
+export default {
+    data() {
+        return { 
+            aaa: 'ABC' 
+        }
+    }
+}
+</script>
