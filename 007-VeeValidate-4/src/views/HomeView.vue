d303221e3b59941c1b51d2a688e0f3a330ee606c
RohimChou <rohim.chou@gmail.com>
2022-10-26 11:00:37 +0800

    vee-validate v4.7 Try dropdown list

--- 007-VeeValidate-4/src/views/HomeView.vue
+++ 007-VeeValidate-4/src/views/HomeView.vue
@@ -2,7 +2,7 @@
 import { Form, Field, ErrorMessage } from 'vee-validate'
 import AllRules from '@vee-validate/rules'
 import { defineRule, configure } from 'vee-validate'
-import { computed, ref } from "vue";
+import { computed, ref, onUpdated } from "vue";
 import { setLocale, localize } from '@vee-validate/i18n';
 
 // import rules
@@ -11,7 +11,7 @@ Object.keys(AllRules).forEach(rule => {
 });
 
 defineRule('positive', (value, limits, event) => {
-  console.log(value, limits, rule);
+  console.log(value, limits, event);
   if (value !== undefined) {
     return value >= 1 ? true : '必須是正數';
   } else {
@@ -37,6 +37,14 @@ configure({
   validateOnModelUpdate: false,
 });
 
+
+// Data
+const data = ref({ brands: ['addidas', 'nike' ], selected: "1" });
+onUpdated(() => {
+  console.log("AAAA");
+  console.log(data.value.selected);
+});
+
 const mySubmit = (formValues) => {
   console.log("mySubmit");
   console.log(formValues);
@@ -59,7 +67,7 @@ const myInvalidSubmit = (formValues) => {
 <template>
   <main>
     <div id="div1">
-      Form1
+      Form1 {{data.selected}}
       <Form v-slot="{ handleSubmit }" as="div" @invalid-submit="myInvalidSubmit">
         <form @submit="handleSubmit($event, mySubmit)" ref="myForm">
           <label for="username">UserName: </label>
@@ -70,6 +78,11 @@ const myInvalidSubmit = (formValues) => {
           <Field type="number" name="birthyear" rules="positive:abc"></Field>
           <ErrorMessage style="color:red" name="birthyear"></ErrorMessage>
 
+          <Field name="field" as="select" v-model="data.selected">
+            <option value="1">Coffee</option>
+            <option value="2">Tea</option>
+            <option value="3">Coke</option>
+          </Field>
           <button id="btn1">this will submit</button>
           <button id="btn2" type="button">this won't</button>
         </form>
